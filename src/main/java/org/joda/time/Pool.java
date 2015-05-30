d23d5f7162f27aa5e585d9299ac3b6a4fd889286
pgs <pgs@tid.es>
2015-05-30 12:14:40 +0200

    add Hours pool to Pool

--- src/main/java/org/joda/time/Pool.java
+++ src/main/java/org/joda/time/Pool.java
@@ -7,10 +7,12 @@ public class Pool {
     private static Pool myInstance;
     private HashMap<Integer, Days> days;
     private HashMap<Integer, Minutes> minutes;
+    private HashMap<Integer, Hours> hours;
 
     private Pool() {
         this.days = new HashMap<Integer, Days>();
         this.minutes = new HashMap<Integer, Minutes>();
+        this.hours = new HashMap<Integer, Hours>();
     }
 
     public static Pool getInstance() {
@@ -50,12 +52,30 @@ public class Pool {
         return (Minutes) result;
     }
 
+    public static Hours retrieveHours(int numeral) {
+
+        Pool pool = Pool.getInstance();
+
+        Object result = pool.getHours(numeral);
+
+        if (result == null) {
+            result =  new Hours(numeral);
+            pool.addHours(numeral, (Hours) result);
+        }
+
+        return (Hours) result;
+    }
+
     private void addDay(int numeral, Days day) {
         days.put(new Integer(numeral), day);
     }
 
     private void addMinutes(int numeral, Minutes minute) {
         minutes.put(new Integer(numeral), minute);
+    }
+
+    private void addHours(int numeral, Hours hour) {
+        hours.put(new Integer(numeral), hour);
     }
 
     private Object getDays(int numeral){
@@ -66,6 +86,12 @@ public class Pool {
 
     private Object getMinutes(int numeral) {
         Object instance = minutes.get(new Integer(numeral));
+
+        return instance;
+    }
+
+    private Object getHours(int numeral) {
+        Object instance = hours.get(new Integer(numeral));
 
         return instance;
     }
